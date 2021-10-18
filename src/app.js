2855dcd07e20646e18c1d974afcec77ca3934a28
Krystal Hortizuela <37311059+jingaling13@users.noreply.github.com>
2021-10-17 22:40:29 -0700

    fixed city display

--- src/app.js
+++ src/app.js
@@ -21,18 +21,19 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("icon");
+    let iconElement = document.querySelector("#icon");
 
     celciusTemperature = response.data.main.temp;
 
     temperatureElement.innerHTML = Math.round (celciusTemperature);
-    cityElement.innerHTML = response.data.main.name;
+    cityElement.innerHTML = response.data.name;
     desctriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src" , `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt" , response.data.weather[0].description);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 function search(city) {
@@ -45,35 +46,34 @@ function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-    console.log(cityInputElement.value);
 }
 
-function displayFarenheitTemperature(event){
+function displayFahrenheitTemperature(event){
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
     celciusLink.classList.remove("active");
-    farenheitLink.classList.add("active");
-    let farenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(farenheitTemperature);
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelciusTemperature(event){
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
+    fahrenheitLink.classList.remove("active");
     celciusLink.classList.add("active");
-    farenheitLink.classList.remove("active");
     let celciusTemperature = Math.round(celciusTemperature);
 }
 
 let celciusTemperature = null;
 
-let form = document.querySelector("selector-form");
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let farenheitLink = document.querySelector("#farenheit-link");
-farenheitLink.addEventListener("click", displayFarenheitTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
-search("New York");
+search("Los Angeles");
