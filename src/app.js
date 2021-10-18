8a9d1c0e6d8064624ec2c133682f484715573118
Krystal Hortizuela <37311059+jingaling13@users.noreply.github.com>
2021-10-17 21:07:15 -0700

    time

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
+function formatDate(timestamp) {
+    //calculate the date
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours <10) {
+        hours =   `=${minutes}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes <10) {
+        minutes =   `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let desctriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.main.name;
     desctriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = response.data.wind.speed;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
