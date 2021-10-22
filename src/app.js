8e5a8d347e21e15750910e5a81227a9aed5a9209
Krystal Hortizuela <37311059+jingaling13@users.noreply.github.com>
2021-10-21 20:22:50 -0700

    format day and loop for forecast

--- src/app.js
+++ src/app.js
@@ -14,34 +14,48 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-    let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    let forecastHTML = `<div class="row">`;
-    let days = ["Thurs", "Fri", "Sat", "Sun"];
-    days.forEach(function(day) {
-        forecastHTML = forecastHTML + `
-        
-                <div class="col-2">
-                    <div class="weather-forecast-date">${day}</div>
-                 <img
-                    src="http://openweathermap.org/img/wn/50d@2x.png"
-                     alt=""
-                     width="42"
-                    />
-                <div class="weather-forecast-temperatures">
-                     <span class="weather-forecast-temperature-max"> 18° </span>
-                     <span class="weather-forecast-temperature-min"> 12° </span>
-                </div>
-                </div>
-    `;
+  return days[day];
+}
 
-    })
+function displayForecast(response) {
+  let forecast = response.data.daily;
 
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
+  let forecastElement = document.querySelector("#forecast");
 
-    
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML + `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates){
@@ -114,4 +128,3 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Los Angeles");
-displayForecast();
