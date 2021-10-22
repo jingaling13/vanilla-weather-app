77684d926498f98d7ab3e70ae246305837ad4ee1
Krystal Hortizuela <37311059+jingaling13@users.noreply.github.com>
2021-10-21 19:51:41 -0700

    get coord for forecast

--- src/app.js
+++ src/app.js
@@ -44,6 +44,12 @@ function displayForecast() {
     
 }
 
+function getForecast(coordinates){
+    let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -64,6 +70,7 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
+    getForecast(response.data.coord);
 }
 
 function search(city) {
